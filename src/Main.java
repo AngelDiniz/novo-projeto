e3ec765a8026e0f3b8b71fd210f180bb9aa11e41
Angel Pereira Diniz <angelpdiniz@gmail.com>
2025-08-19 19:13:58 -0300

    Nova conta criada teste

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,6 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Ana","1.222.333-44");
-        Conta contaDaAna = new Conta(cliente, 500.0);
-        Cliente cliente2 = new Cliente("Angel","1111111111");
-        System.out.println("--- Dados Iniciais da Conta ---");
-        System.out.println("Titular: " + contaDaAna.getTitular().getNome());
-        System.out.println("CPF: " + contaDaAna.getTitular().getCpf());
-        System.out.println("Saldo: R$" + contaDaAna.getSaldo());
 
-
-        System.out.println("\n--- Realizando Operacoes ---");
-
-        contaDaAna.depositar(300.0);
-
-        contaDaAna.sacar(150.0);
-
-        contaDaAna.sacar(1000.0);
-        Conta contaDaAngel = new Conta(cliente2, 500.0);;
-        System.out.println("\n--- Saldo Final ---");
-        System.out.println("Saldo atual: R$" + contaDaAna.getSaldo());
-        System.out.print("Nova conta criada"+contaDaAngel.getTitular().getNome());
 }}
 
