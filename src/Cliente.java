3285fa22dda17b7c92791b17a28602631e1f725c
Angel Pereira Diniz <angelpdiniz@gmail.com>
2025-08-19 17:41:18 -0300

    Classe Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+    private nome;
+    private cpf;
+
+public Cliente (String nome,String cpf){
+    this.nome;
+    this.cpf;
+
+    public String getNome(){
+        return nome;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+}
+}
