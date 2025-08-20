d57fe6c65502fd8ca89f261dfc4f194bc617bec1
Angel Pereira Diniz <angelpdiniz@gmail.com>
2025-08-20 16:53:41 -0300

    Ajustes na Classe Main criando contas.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,20 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
 
-}}
+        Cliente cliente = new Cliente("Angel", "1111111111");
+        Conta contaDaAngel = new Conta(cliente, 500.0);
+
+
+        System.out.print("Nova conta criada " + contaDaAngel.getTitular().getNome());
+        System.out.println("\n---Inicial ---");
+        contaDaAngel.depositar(500);
+        contaDaAngel.sacar(200);
+
+
+        System.out.println(contaDaAngel.getSaldo());
+
+
+        }
+
+}
 
