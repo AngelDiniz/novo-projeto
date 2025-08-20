1efb51ae4b426b4e38da275d833126ca0e9cae07
Angel Pereira Diniz <angelpdiniz@gmail.com>
2025-08-20 17:41:55 -0300

    melhorias no Historico

--- src/Historico.java
+++ src/Historico.java
@@ -4,17 +4,14 @@ import java.util.List;
 public class Historico {
     private List<String> nomesClientes;
 
-    // O construtor inicializa a lista
     public Historico() {
         this.nomesClientes = new ArrayList<>();
     }
 
-    // Método para adicionar um nome de cliente à lista
     public void adicionarNome(String nome) {
         this.nomesClientes.add(nome);
     }
 
-    // Método para exibir o histórico de nomes
     public void mostrarHistorico() {
         System.out.println("\n--- Histórico de Nomes de Clientes ---");
         if (nomesClientes.isEmpty()) {
